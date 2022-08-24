71d9e88d3b06e546d189bcd4742b55df84d6f988
Matthew Bernardo <matthew@e-bot7.com>
2022-08-24 11:40:34 +0200

    Make terrain navigable

--- src/scripts/classes/Terrainosaurus.ts
+++ src/scripts/classes/Terrainosaurus.ts
@@ -1,6 +1,6 @@
 import { BufferGeometry, Float32BufferAttribute } from "three";
 
-const VERTICES_PER_SQUARE = 6 // This will change to 4 if/when we do some memory optimizations
+const VERTICES_PER_SQUARE = 6; // This will change to 4 if/when we do some memory optimizations
 
 export interface ITerrainosaurusProps {
   size: number;
@@ -34,7 +34,7 @@ export class Terrainosaurus {
     this.indices = [];
     this.size = props.size;
     this.offset = props.size / 2;
-    this.setInitialVertices(this.offset)
+    this.setInitialVertices(this.offset);
   }
   setInitialVertices(offset: number) {
     // Initialize such that we can use diamond-square displacement
@@ -53,21 +53,20 @@ export class Terrainosaurus {
       .map((v) => ({ ...v, pos: v.pos.map((p) => p * offset) }));
   }
   recurseFullMap(levels = 1) {
-  for (let level = 0; level < levels; level++) {
-    // Adds one level of recursion across the entire terrain map
-    for (let i = this.vertices.length - 6; i > -1; i -= 6) {
-      this.recursivelyGenerate(i)
+    for (let level = 0; level < levels; level++) {
+      // Adds one level of recursion across the entire terrain map
+      for (let i = this.vertices.length - 6; i > -1; i -= 6) {
+        this.recursivelyGenerate(i);
+      }
     }
   }
-}
   recurseSection({ vertices, absoluteIndex }: ISection, levels: number = 1) {
     for (let level = 0; level < levels; level++) {
-    // Adds one level of recursion across the entire terrain map
-    for (let i = vertices.length - 6; i > -1; i -= 6) {
-      this.recursivelyGenerate(i + absoluteIndex)
+      // Adds one level of recursion across the entire terrain map
+      for (let i = vertices.length - 6; i > -1; i -= 6) {
+        this.recursivelyGenerate(i + absoluteIndex);
+      }
     }
-  }
-
   }
   recursivelyGenerate(vertexIndex: number) {
     /**
@@ -75,37 +74,70 @@ export class Terrainosaurus {
      * Each square is represented by 6 vertices, so we can get a slice from the vertices array, create a new one, and insert it.
      */
     if (vertexIndex % VERTICES_PER_SQUARE) {
-      throw new Error("The given vertex does not represent the start of a cell")
+      throw new Error(
+        "The given vertex does not represent the start of a cell"
+      );
     }
     if (!this.vertices[vertexIndex]) {
-      console.warn(`Skipping recursion: Index ${vertexIndex} exceeds bounds (${this.vertices.length})`)
-      return
+      console.warn(
+        `Skipping recursion: Index ${vertexIndex} exceeds bounds (${this.vertices.length})`
+      );
+      return;
     }
-    const verticesToReplace = this.vertices.slice(vertexIndex, vertexIndex + VERTICES_PER_SQUARE)
-    const recursions = this.vertices[vertexIndex].recursions + 1
+    const verticesToReplace = this.vertices.slice(
+      vertexIndex,
+      vertexIndex + VERTICES_PER_SQUARE
+    );
+    const recursions = this.vertices[vertexIndex].recursions + 1;
     // Get 4 corners of the square to compute the centroid.
-    let replacementVertices = getSubSquares({ vertices: verticesToReplace, recursions })
-    
-    this.vertices.splice(vertexIndex, VERTICES_PER_SQUARE, ...replacementVertices)
+    let replacementVertices = getSubSquares({
+      vertices: verticesToReplace,
+      recursions,
+    });
+
+    this.vertices.splice(
+      vertexIndex,
+      VERTICES_PER_SQUARE,
+      ...replacementVertices
+    );
   }
   getSection(path: Array<1 | 2 | 3 | 4>, section?: ISection): ISection {
-    section = section || { vertices: this.vertices, absoluteIndex: 0 }
+    section = section || { vertices: this.vertices, absoluteIndex: 0 };
     return path.reduce((acc, quadrant: number, level) => {
-      const q1Index = 0 // Top left
-      const q2Index = Math.pow((acc.vertices[0].recursions - level), 2) * VERTICES_PER_SQUARE // Top right
-      const q3Index = q2Index + Math.pow(acc.vertices[q2Index].recursions - level, 2) * VERTICES_PER_SQUARE // Bottom left
-      const q4Index = q3Index + Math.pow(acc.vertices[q3Index].recursions - level, 2) * VERTICES_PER_SQUARE // Bottom left
-      const endIndex = q4Index + Math.pow(acc.vertices[q3Index].recursions - level, 2) * VERTICES_PER_SQUARE // End of quadrant
-      const quadrantIndices: Array<number> = [q1Index, q2Index, q3Index, q4Index, endIndex]
+      const q1Index = 0; // Top left
+      const q2Index =
+        Math.pow(acc.vertices[0].recursions - level, 2) * VERTICES_PER_SQUARE; // Top right
+      const q3Index =
+        q2Index +
+        Math.pow(acc.vertices[q2Index].recursions - level, 2) *
+          VERTICES_PER_SQUARE; // Bottom left
+      const q4Index =
+        q3Index +
+        Math.pow(acc.vertices[q3Index].recursions - level, 2) *
+          VERTICES_PER_SQUARE; // Bottom left
+      const endIndex =
+        q4Index +
+        Math.pow(acc.vertices[q3Index].recursions - level, 2) *
+          VERTICES_PER_SQUARE; // End of quadrant
+      const quadrantIndices: Array<number> = [
+        q1Index,
+        q2Index,
+        q3Index,
+        q4Index,
+        endIndex,
+      ];
       // Weird step to account for the fact that the vertex rendering order doesn't match
       // conventional quadrant order in mathematics
       if (quadrant < 3) {
-        quadrant = quadrant % 2 + 1 // 1 -> 2, 2 -> 1
+        quadrant = (quadrant % 2) + 1; // 1 -> 2, 2 -> 1
       }
-      acc.vertices = acc.vertices.slice(quadrantIndices[quadrant - 1], quadrantIndices[quadrant])
-      acc.absoluteIndex += quadrantIndices[quadrant - 1]
-      return acc
-    }, section)
+      acc.vertices = acc.vertices.slice(
+        quadrantIndices[quadrant - 1],
+        quadrantIndices[quadrant]
+      );
+      acc.absoluteIndex += quadrantIndices[quadrant - 1];
+      return acc;
+    }, section);
   }
   createGeometry(section: Array<IVertex> = this.vertices) {
     // When called, generates a BufferGeometry out of the current vertices
@@ -129,7 +161,10 @@ export class Terrainosaurus {
     // Use parallel arrays to create BufferGeometry
     geometry.setAttribute(
       "position",
-      new Float32BufferAttribute(new Float32Array(positions), positionNumComponents)
+      new Float32BufferAttribute(
+        new Float32Array(positions),
+        positionNumComponents
+      )
     );
     geometry.setAttribute(
       "normal",
@@ -145,78 +180,144 @@ export class Terrainosaurus {
 }
 
 interface IGetSquareProps {
-  vertices: Array<IVertex>
-  recursions: number
+  vertices: Array<IVertex>;
+  recursions: number;
 }
 
 /**
- * 
+ *
  * @param props The set of vertices that need to be replaced
  * @returns An array of vertices that represent 4 squares that cover the same area as the original vertices
  */
 function getSubSquares(props: IGetSquareProps): Array<IVertex> {
-  const { recursions } = props
-  const [bottomRight, bottomLeft, topRight, _, __, topLeft] = props.vertices
+  const { recursions } = props;
+  const [bottomRight, bottomLeft, topRight, _, __, topLeft] = props.vertices;
 
   const center = bilinearInterpolation({
     p1: bottomRight,
     p2: bottomLeft,
     p3: topRight,
-    p4: topLeft
-  })
+    p4: topLeft,
+  });
 
-  const maxDisplacement = (bottomRight.pos[0] - bottomLeft.pos[0]) / 1.5
-  center.y += Math.random() * maxDisplacement - maxDisplacement / 2
-  
-  const baseVertex = { norm: [0,1,0], uv: [0,1], recursions }
+  const maxDisplacement = (bottomRight.pos[0] - bottomLeft.pos[0]) / 1.5;
+  center.y += Math.random() * maxDisplacement - maxDisplacement / 2;
+
+  const baseVertex = { norm: [0, 1, 0], uv: [0, 1], recursions };
   const newVertices = [
     // Top left square
     { pos: [center.x, center.y, center.z], ...baseVertex },
-    { pos: [topLeft.pos[0], (topLeft.pos[1] + bottomLeft.pos[1]) / 2, center.z], ...baseVertex },
-    { pos: [center.x, (topLeft.pos[1] + topRight.pos[1]) / 2, topLeft.pos[2]], ...baseVertex },
+    {
+      pos: [topLeft.pos[0], (topLeft.pos[1] + bottomLeft.pos[1]) / 2, center.z],
+      ...baseVertex,
+    },
+    {
+      pos: [center.x, (topLeft.pos[1] + topRight.pos[1]) / 2, topLeft.pos[2]],
+      ...baseVertex,
+    },
 
-    { pos: [center.x, (topLeft.pos[1] + topRight.pos[1]) / 2, topLeft.pos[2]], ...baseVertex },
-    { pos: [topLeft.pos[0], (topLeft.pos[1] + bottomLeft.pos[1]) / 2, center.z], ...baseVertex },
+    {
+      pos: [center.x, (topLeft.pos[1] + topRight.pos[1]) / 2, topLeft.pos[2]],
+      ...baseVertex,
+    },
+    {
+      pos: [topLeft.pos[0], (topLeft.pos[1] + bottomLeft.pos[1]) / 2, center.z],
+      ...baseVertex,
+    },
     topLeft,
-    
+
     // Top right square
-    { pos: [topRight.pos[0], (topRight.pos[1] + bottomRight.pos[1]) / 2, center.z], ...baseVertex },
+    {
+      pos: [
+        topRight.pos[0],
+        (topRight.pos[1] + bottomRight.pos[1]) / 2,
+        center.z,
+      ],
+      ...baseVertex,
+    },
     { pos: [center.x, center.y, center.z], ...baseVertex },
     topRight,
 
     topRight,
     { pos: [center.x, center.y, center.z], ...baseVertex },
-    { pos: [center.x, (topLeft.pos[1] + topRight.pos[1]) / 2, topRight.pos[2]], ...baseVertex },
-
+    {
+      pos: [center.x, (topLeft.pos[1] + topRight.pos[1]) / 2, topRight.pos[2]],
+      ...baseVertex,
+    },
 
     // Bottom left square
-    { pos: [center.x, (bottomLeft.pos[1] + bottomRight.pos[1]) / 2, bottomLeft.pos[2]], ...baseVertex },
+    {
+      pos: [
+        center.x,
+        (bottomLeft.pos[1] + bottomRight.pos[1]) / 2,
+        bottomLeft.pos[2],
+      ],
+      ...baseVertex,
+    },
     bottomLeft,
     { pos: [center.x, center.y, center.z], ...baseVertex },
 
     { pos: [center.x, center.y, center.z], ...baseVertex },
     bottomLeft,
-    { pos: [topLeft.pos[0], (topLeft.pos[1] + bottomLeft.pos[1]) / 2, center.z], ...baseVertex },
-
+    {
+      pos: [topLeft.pos[0], (topLeft.pos[1] + bottomLeft.pos[1]) / 2, center.z],
+      ...baseVertex,
+    },
 
     // Bottom right square
     bottomRight,
-    { pos: [center.x, (bottomLeft.pos[1] + bottomRight.pos[1]) / 2, bottomLeft.pos[2]], ...baseVertex },
-    { pos: [topRight.pos[0], (topRight.pos[1] + bottomRight.pos[1]) / 2, center.z], ...baseVertex },
-    
-    { pos: [topRight.pos[0], (topRight.pos[1] + bottomRight.pos[1]) / 2, center.z], ...baseVertex },
-    { pos: [center.x, (bottomLeft.pos[1] + bottomRight.pos[1]) / 2, bottomLeft.pos[2]], ...baseVertex },
-    { pos: [center.x, center.y, center.z], ...baseVertex },
-  ]
+    {
+      pos: [
+        center.x,
+        (bottomLeft.pos[1] + bottomRight.pos[1]) / 2,
+        bottomLeft.pos[2],
+      ],
+      ...baseVertex,
+    },
+    {
+      pos: [
+        topRight.pos[0],
+        (topRight.pos[1] + bottomRight.pos[1]) / 2,
+        center.z,
+      ],
+      ...baseVertex,
+    },
 
-  return newVertices
+    {
+      pos: [
+        topRight.pos[0],
+        (topRight.pos[1] + bottomRight.pos[1]) / 2,
+        center.z,
+      ],
+      ...baseVertex,
+    },
+    {
+      pos: [
+        center.x,
+        (bottomLeft.pos[1] + bottomRight.pos[1]) / 2,
+        bottomLeft.pos[2],
+      ],
+      ...baseVertex,
+    },
+    { pos: [center.x, center.y, center.z], ...baseVertex },
+  ];
+
+  return newVertices;
 }
 
-function getSquare(topLeft: IVertex, size: number, recursions: number): Array<IVertex> {
-  const baseVertex = { norm: [0,1,0], uv: [0,1], recursions: recursions + 1 }
+function getSquare(
+  topLeft: IVertex,
+  size: number,
+  recursions: number
+): Array<IVertex> {
+  const baseVertex = {
+    norm: [0, 1, 0],
+    uv: [0, 1],
+    recursions: recursions + 1,
+  };
   return [
-    { pos: [], ...baseVertex } // bottom right
-  ]
+    { pos: [], ...baseVertex }, // bottom right
+  ];
 }
 
 interface IBilinearInterpolationProps {
@@ -224,23 +325,29 @@ interface IBilinearInterpolationProps {
   p2: IVertex;
   p3: IVertex;
   p4: IVertex;
-  isCentroid?: boolean
+  isCentroid?: boolean;
 }
-function bilinearInterpolation(props: IBilinearInterpolationProps): { x: number, y: number, z: number } {
-  /** 
+function bilinearInterpolation(props: IBilinearInterpolationProps): {
+  x: number;
+  y: number;
+  z: number;
+} {
+  /**
    * Get bilinear interpolated y-value using weighted mean method.
    * Calculate distances from each point and normalize to get weights.
    * Return weighted average.
    */
-  const {p1, p2, p3, p4, isCentroid = true} = props
+  const { p1, p2, p3, p4, isCentroid = true } = props;
   if (isCentroid) {
     // We expect to always do the bilinear interpolation for the centroid.
     // This means the weights will all be 0.25 - no need for expensive distance calculations
-    const x = (p1.pos[0] + p2.pos[0] + p3.pos[0] + p4.pos[0]) / 4
-    const y = (p1.pos[1] + p2.pos[1] + p3.pos[1] + p4.pos[1]) / 4
-    const z = (p1.pos[2] + p2.pos[2] + p3.pos[2] + p4.pos[2]) / 4
-    return { x, y, z }
+    const x = (p1.pos[0] + p2.pos[0] + p3.pos[0] + p4.pos[0]) / 4;
+    const y = (p1.pos[1] + p2.pos[1] + p3.pos[1] + p4.pos[1]) / 4;
+    const z = (p1.pos[2] + p2.pos[2] + p3.pos[2] + p4.pos[2]) / 4;
+    return { x, y, z };
   } else {
-    throw new Error("Oops, someone had better implement non-centroid bilinear interpolation")
+    throw new Error(
+      "Oops, someone had better implement non-centroid bilinear interpolation"
+    );
   }
 }
--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,23 +1,56 @@
 import { Terrainosaurus } from "./classes/Terrainosaurus";
-import { registerGeometry } from "aframe";
+import { registerComponent, registerGeometry } from "aframe";
+import { Raycaster, Vector3 } from "three";
 
 const terrainClient = new Terrainosaurus({
   size: 20,
   seed: 0,
   lowDetailRecursions: 0,
-  highDetailRecursions: 0
-})
+  highDetailRecursions: 0,
+});
 
-terrainClient.recurseFullMap()
+terrainClient.recurseFullMap();
 
-terrainClient.recurseSection(terrainClient.getSection([3]))
-terrainClient.recurseSection(terrainClient.getSection([3, 1]))
+terrainClient.recurseSection(terrainClient.getSection([3]));
+terrainClient.recurseSection(terrainClient.getSection([3, 1]));
 
-const geometry = terrainClient.createGeometry()
+const geometry = terrainClient.createGeometry();
 registerGeometry("terrainosaurus-terrain", {
   init() {
     geometry.computeBoundingSphere();
     geometry.computeVertexNormals();
-    this.geometry = geometry
+    this.geometry = geometry;
+  },
+});
+
+registerComponent("navigable-terrain", {
+  init() {
+    this.camera = document.querySelector("[camera]");
+    this.cameraWorldPosition = new Vector3();
+    this.UP_VECTOR = new Vector3(0, 1, 0);
+    this.DOWN_VECTOR = new Vector3(0, -1, 0);
+    this.intersections = [];
+    this.raycaster = new Raycaster(this.cameraWorldPosition, this.DOWN_VECTOR, 0, 100);
+  },
+  tick() {
+    this.camera.object3D.getWorldPosition(this.cameraWorldPosition);
+    this.raycaster.set(this.cameraWorldPosition, this.DOWN_VECTOR);
+    this.raycaster.intersectObject(this.el.object3D, true, this.intersections);
+    this.handleIntersection()
+  },
+  handleIntersection() {
+    if (this.intersections.length) {
+      // Basic PD control - not a proper physics sim with gravity
+      const yGround = this.intersections[0].point.y
+      const yCamera = this.cameraWorldPosition.y
+      const controlInput = 0.2 * (yGround - yCamera + 1.2)
+      
+      // Note that we shift the ground, not camera. This makes AR mode work better
+      this.el.object3D.position.y = this.el.object3D.position.y - controlInput
+    
+      this.intersections = []
+      return true
+    }
+    return false
   }
-})
+});
