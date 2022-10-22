3ba55f97f1ae84ffb2a94dac79295a7f335e81be
Matthew Bernardo <matthew@e-bot7.com>
2022-10-22 18:03:49 +0200

    Fix function reconstruction bug caused by uglify options

--- dist/Terrainosaurus.js
+++ dist/Terrainosaurus.js
@@ -1,1 +1,289 @@
-import{BufferGeometry,Float32BufferAttribute,Vector3}from"three";import{defaultGenerators,defaultGeneratorSelector}from"./generators";import{SimplexNoise}from"simplex-noise-esm";const VERTICES_PER_SQUARE=6;class Terrainosaurus{THREE;size;seed;offset;vertices;indices;state;colors;waterLevel;generators;generatorSelector;vertexWorker;vertexWorkerUrl;constructor(e){this.vertices=[],this.state=e.state||{},this.indices=[],this.size=e.size,this.offset=e.size/2,this.generatorSelector=e.generatorSelector||defaultGeneratorSelector,this.generators=e.generators||defaultGenerators,this.vertexWorkerUrl=e.vertexWorkerUrl,this.seed=(e.seed||Math.random()).toString(),this.colors=e.colors,this.waterLevel=e.waterLevel,this.state.simplex=new SimplexNoise(this.seed),this.setInitialVertices(this.offset),this.THREE={Vector3:Vector3}}setInitialVertices(t){this.vertices=this.vertices.concat([{pos:[1,0,1],norm:[0,1,0],uv:[0,1],recursions:0},{pos:[-1,0,1],norm:[0,1,0],uv:[1,1],recursions:0},{pos:[1,0,-1],norm:[0,1,0],uv:[0,0],recursions:0},{pos:[1,0,-1],norm:[0,1,0],uv:[0,0],recursions:0},{pos:[-1,0,1],norm:[0,1,0],uv:[1,1],recursions:0},{pos:[-1,0,-1],norm:[0,1,0],uv:[1,0],recursions:0}]).map(e=>({...e,pos:e.pos.map(e=>e*t)}))}recurseFullMap(t=1){for(let e=0;e<t;e++)for(let e=this.vertices.length-6;-1<e;e-=6)this.recursivelyGenerate(e)}recurseSection({vertices:t,absoluteIndex:r},s=1){for(let e=0;e<s;e++)for(let e=t.length-6;-1<e;e-=6)this.recursivelyGenerate(e+r)}async recurseSectionInBackground(s,o=1){return new Promise((t,e)=>{this.vertexWorkerUrl||e("Cannot recurse in background - vertexWorkerUrl not provided in constructor");e=new Worker(this.vertexWorkerUrl,{type:"module"});const r={start:s.absoluteIndex,end:s.vertices.length};e.onmessage=e=>{this.vertices.splice(r.start,r.end,...e.data.vertices),t(e.data.geometry)},e.postMessage({action:"recurseSection",seed:this.seed,colors:this.colors,waterLevel:this.waterLevel,section:{vertices:s.vertices,absoluteIndex:0},generatorSelector:this.generatorSelector.toString(),generators:this.generators.map(e=>e.toString()),levels:o})})}recursivelyGenerate(e){if(e%VERTICES_PER_SQUARE)throw new Error("The given vertex does not represent the start of a cell");var t,r;this.vertices[e]?(r=this.vertices.slice(e,e+VERTICES_PER_SQUARE),t=this.vertices[e].recursions+1,r=this.getSubSquares({vertices:r,recursions:t,vertexIndex:e}),this.vertices.splice(e,VERTICES_PER_SQUARE,...r)):console.warn(`Skipping recursion: Index ${e} exceeds bounds (${this.vertices.length})`)}getSection(e,t){return t=t||{vertices:this.vertices,absoluteIndex:0},e.reduce((e,t,r)=>{var r=r+1,s=Math.pow(4,e.vertices[0].recursions-r)*VERTICES_PER_SQUARE,o=s+Math.pow(4,e.vertices[s].recursions-r)*VERTICES_PER_SQUARE,n=o+Math.pow(4,e.vertices[o].recursions-r)*VERTICES_PER_SQUARE,s=[0,s,o,n,n+Math.pow(4,e.vertices[o].recursions-r)*VERTICES_PER_SQUARE];return e.vertices=e.vertices.slice(s[(t=t<3?t%2+1:t)-1],s[t]),e.absoluteIndex+=s[t-1],e},t)}getSubSquares(e){var t=e["recursions"],[r,s,o,,,n]=e.vertices,i=bilinearInterpolation({p1:r,p2:s,p3:o,p4:n}),t=vertexGenerator(t),i=[{pos:[i.x,i.y,i.z],...t.next().value},{pos:[n.pos[0],(n.pos[1]+s.pos[1])/2,i.z],...t.next().value},{pos:[i.x,(n.pos[1]+o.pos[1])/2,n.pos[2]],...t.next().value},{pos:[i.x,(n.pos[1]+o.pos[1])/2,n.pos[2]],...t.next().value},{pos:[n.pos[0],(n.pos[1]+s.pos[1])/2,i.z],...t.next().value},{...n,...t.next().value},{pos:[o.pos[0],(o.pos[1]+r.pos[1])/2,i.z],...t.next().value},{pos:[i.x,i.y,i.z],...t.next().value},{...o,...t.next().value},{...o,...t.next().value},{pos:[i.x,i.y,i.z],...t.next().value},{pos:[i.x,(n.pos[1]+o.pos[1])/2,o.pos[2]],...t.next().value},{pos:[i.x,(s.pos[1]+r.pos[1])/2,s.pos[2]],...t.next().value},{...s,...t.next().value},{pos:[i.x,i.y,i.z],...t.next().value},{pos:[i.x,i.y,i.z],...t.next().value},{...s,...t.next().value},{pos:[n.pos[0],(n.pos[1]+s.pos[1])/2,i.z],...t.next().value},{...r,...t.next().value},{pos:[i.x,(s.pos[1]+r.pos[1])/2,s.pos[2]],...t.next().value},{pos:[o.pos[0],(o.pos[1]+r.pos[1])/2,i.z],...t.next().value},{pos:[o.pos[0],(o.pos[1]+r.pos[1])/2,i.z],...t.next().value},{pos:[i.x,(s.pos[1]+r.pos[1])/2,s.pos[2]],...t.next().value},{pos:[i.x,i.y,i.z],...t.next().value}];return this.generators[this.generatorSelector({topLeft:n,topRight:o,bottomLeft:s,bottomRight:r,vertexIndex:e.vertexIndex})].call(this,i,i.map(e=>this.state.simplex.noise2D(e.pos[0],e.pos[2]))),i}createGeometry(e=this.vertices){var t=new BufferGeometry,{positions:e,normals:r,uvs:s,colors:o}=e.reduce((e,t)=>(e.positions=e.positions.concat(t.pos),e.normals=e.normals.concat(t.norm),e.uvs=e.uvs.concat(t.uv),e.colors=e.colors.concat(t.color),e),{positions:[],normals:[],uvs:[],colors:[]});return t.setAttribute("position",new Float32BufferAttribute(new Float32Array(e),3)),t.setAttribute("normal",new Float32BufferAttribute(new Float32Array(r),3)),t.setAttribute("uv",new Float32BufferAttribute(new Float32Array(s),2)),t.setAttribute("color",new Float32BufferAttribute(new Float32Array(o),3)),t}}function bilinearInterpolation(e){var{p1:e,p2:t,p3:r,p4:s,isCentroid:o=!0}=e;if(o)return{x:(e.pos[0]+t.pos[0]+r.pos[0]+s.pos[0])/4,y:(e.pos[1]+t.pos[1]+r.pos[1]+s.pos[1])/4,z:(e.pos[2]+t.pos[2]+r.pos[2]+s.pos[2])/4};throw new Error("Oops, someone had better implement non-centroid bilinear interpolation")}const vertexGenerator=function*(e){for(;;)yield{norm:[0,1,0],uv:[0,1],recursions:e},yield{norm:[0,1,0],uv:[1,1],recursions:e},yield{norm:[0,1,0],uv:[0,0],recursions:e},yield{norm:[0,1,0],uv:[0,0],recursions:e},yield{norm:[0,1,0],uv:[1,1],recursions:e},yield{norm:[0,1,0],uv:[1,0],recursions:e}};export{Terrainosaurus};
+import {
+    BufferGeometry,
+    Float32BufferAttribute,
+    Vector3
+} from "three";
+
+import {
+    defaultGenerators,
+    defaultGeneratorSelector
+} from "./generators";
+
+import {
+    SimplexNoise
+} from "simplex-noise-esm";
+
+const VERTICES_PER_SQUARE = 6;
+
+class Terrainosaurus {
+    THREE;
+    size;
+    seed;
+    offset;
+    vertices;
+    indices;
+    state;
+    colors;
+    waterLevel;
+    generators;
+    generatorSelector;
+    vertexWorker;
+    vertexWorkerUrl;
+    constructor(e) {
+        this.vertices = [], this.state = e.state || {}, this.indices = [], this.size = e.size, 
+        this.offset = e.size / 2, this.generatorSelector = e.generatorSelector || defaultGeneratorSelector, 
+        this.generators = e.generators || defaultGenerators, this.vertexWorkerUrl = e.vertexWorkerUrl, 
+        this.seed = (e.seed || Math.random()).toString(), this.colors = e.colors, 
+        this.waterLevel = e.waterLevel, this.state.simplex = new SimplexNoise(this.seed), 
+        this.setInitialVertices(this.offset), this.THREE = {
+            Vector3: Vector3
+        };
+    }
+    setInitialVertices(t) {
+        this.vertices = this.vertices.concat([ {
+            pos: [ 1, 0, 1 ],
+            norm: [ 0, 1, 0 ],
+            uv: [ 0, 1 ],
+            recursions: 0
+        }, {
+            pos: [ -1, 0, 1 ],
+            norm: [ 0, 1, 0 ],
+            uv: [ 1, 1 ],
+            recursions: 0
+        }, {
+            pos: [ 1, 0, -1 ],
+            norm: [ 0, 1, 0 ],
+            uv: [ 0, 0 ],
+            recursions: 0
+        }, {
+            pos: [ 1, 0, -1 ],
+            norm: [ 0, 1, 0 ],
+            uv: [ 0, 0 ],
+            recursions: 0
+        }, {
+            pos: [ -1, 0, 1 ],
+            norm: [ 0, 1, 0 ],
+            uv: [ 1, 1 ],
+            recursions: 0
+        }, {
+            pos: [ -1, 0, -1 ],
+            norm: [ 0, 1, 0 ],
+            uv: [ 1, 0 ],
+            recursions: 0
+        } ]).map(e => ({
+            ...e,
+            pos: e.pos.map(e => e * t)
+        }));
+    }
+    recurseFullMap(t = 1) {
+        for (let e = 0; e < t; e++) for (let e = this.vertices.length - 6; -1 < e; e -= 6) this.recursivelyGenerate(e);
+    }
+    recurseSection({
+        vertices: t,
+        absoluteIndex: r
+    }, s = 1) {
+        for (let e = 0; e < s; e++) for (let e = t.length - 6; -1 < e; e -= 6) this.recursivelyGenerate(e + r);
+    }
+    async recurseSectionInBackground(s, o = 1) {
+        return new Promise((t, e) => {
+            this.vertexWorkerUrl || e("Cannot recurse in background - vertexWorkerUrl not provided in constructor");
+            e = new Worker(this.vertexWorkerUrl, {
+                type: "module"
+            });
+            const r = {
+                start: s.absoluteIndex,
+                end: s.vertices.length
+            };
+            e.onmessage = e => {
+                this.vertices.splice(r.start, r.end, ...e.data.vertices), t(e.data.geometry);
+            }, e.postMessage({
+                action: "recurseSection",
+                seed: this.seed,
+                colors: this.colors,
+                waterLevel: this.waterLevel,
+                section: {
+                    vertices: s.vertices,
+                    absoluteIndex: 0
+                },
+                generatorSelector: this.generatorSelector.toString(),
+                generators: this.generators.map(e => e.toString()),
+                levels: o
+            });
+        });
+    }
+    recursivelyGenerate(e) {
+        if (e % VERTICES_PER_SQUARE) throw new Error("The given vertex does not represent the start of a cell");
+        var t, r;
+        this.vertices[e] ? (r = this.vertices.slice(e, e + VERTICES_PER_SQUARE), 
+        t = this.vertices[e].recursions + 1, r = this.getSubSquares({
+            vertices: r,
+            recursions: t,
+            vertexIndex: e
+        }), this.vertices.splice(e, VERTICES_PER_SQUARE, ...r)) : console.warn(`Skipping recursion: Index ${e} exceeds bounds (${this.vertices.length})`);
+    }
+    getSection(e, t) {
+        return t = t || {
+            vertices: this.vertices,
+            absoluteIndex: 0
+        }, e.reduce((e, t, r) => {
+            var r = r + 1, s = Math.pow(4, e.vertices[0].recursions - r) * VERTICES_PER_SQUARE, o = s + Math.pow(4, e.vertices[s].recursions - r) * VERTICES_PER_SQUARE, n = o + Math.pow(4, e.vertices[o].recursions - r) * VERTICES_PER_SQUARE, s = [ 0, s, o, n, n + Math.pow(4, e.vertices[o].recursions - r) * VERTICES_PER_SQUARE ];
+            return e.vertices = e.vertices.slice(s[(t = t < 3 ? t % 2 + 1 : t) - 1], s[t]), 
+            e.absoluteIndex += s[t - 1], e;
+        }, t);
+    }
+    getSubSquares(e) {
+        var t = e["recursions"], [ r, s, o, , , n ] = e.vertices, i = bilinearInterpolation({
+            p1: r,
+            p2: s,
+            p3: o,
+            p4: n
+        }), t = vertexGenerator(t), i = [ {
+            pos: [ i.x, i.y, i.z ],
+            ...t.next().value
+        }, {
+            pos: [ n.pos[0], (n.pos[1] + s.pos[1]) / 2, i.z ],
+            ...t.next().value
+        }, {
+            pos: [ i.x, (n.pos[1] + o.pos[1]) / 2, n.pos[2] ],
+            ...t.next().value
+        }, {
+            pos: [ i.x, (n.pos[1] + o.pos[1]) / 2, n.pos[2] ],
+            ...t.next().value
+        }, {
+            pos: [ n.pos[0], (n.pos[1] + s.pos[1]) / 2, i.z ],
+            ...t.next().value
+        }, {
+            ...n,
+            ...t.next().value
+        }, {
+            pos: [ o.pos[0], (o.pos[1] + r.pos[1]) / 2, i.z ],
+            ...t.next().value
+        }, {
+            pos: [ i.x, i.y, i.z ],
+            ...t.next().value
+        }, {
+            ...o,
+            ...t.next().value
+        }, {
+            ...o,
+            ...t.next().value
+        }, {
+            pos: [ i.x, i.y, i.z ],
+            ...t.next().value
+        }, {
+            pos: [ i.x, (n.pos[1] + o.pos[1]) / 2, o.pos[2] ],
+            ...t.next().value
+        }, {
+            pos: [ i.x, (s.pos[1] + r.pos[1]) / 2, s.pos[2] ],
+            ...t.next().value
+        }, {
+            ...s,
+            ...t.next().value
+        }, {
+            pos: [ i.x, i.y, i.z ],
+            ...t.next().value
+        }, {
+            pos: [ i.x, i.y, i.z ],
+            ...t.next().value
+        }, {
+            ...s,
+            ...t.next().value
+        }, {
+            pos: [ n.pos[0], (n.pos[1] + s.pos[1]) / 2, i.z ],
+            ...t.next().value
+        }, {
+            ...r,
+            ...t.next().value
+        }, {
+            pos: [ i.x, (s.pos[1] + r.pos[1]) / 2, s.pos[2] ],
+            ...t.next().value
+        }, {
+            pos: [ o.pos[0], (o.pos[1] + r.pos[1]) / 2, i.z ],
+            ...t.next().value
+        }, {
+            pos: [ o.pos[0], (o.pos[1] + r.pos[1]) / 2, i.z ],
+            ...t.next().value
+        }, {
+            pos: [ i.x, (s.pos[1] + r.pos[1]) / 2, s.pos[2] ],
+            ...t.next().value
+        }, {
+            pos: [ i.x, i.y, i.z ],
+            ...t.next().value
+        } ];
+        return this.generators[this.generatorSelector({
+            topLeft: n,
+            topRight: o,
+            bottomLeft: s,
+            bottomRight: r,
+            vertexIndex: e.vertexIndex
+        })].call(this, i, i.map(e => this.state.simplex.noise2D(e.pos[0], e.pos[2]))), 
+        i;
+    }
+    createGeometry(e = this.vertices) {
+        var t = new BufferGeometry(), {
+            positions: e,
+            normals: r,
+            uvs: s,
+            colors: o
+        } = e.reduce((e, t) => (e.positions = e.positions.concat(t.pos), e.normals = e.normals.concat(t.norm), 
+        e.uvs = e.uvs.concat(t.uv), e.colors = e.colors.concat(t.color), e), {
+            positions: [],
+            normals: [],
+            uvs: [],
+            colors: []
+        });
+        return t.setAttribute("position", new Float32BufferAttribute(new Float32Array(e), 3)), 
+        t.setAttribute("normal", new Float32BufferAttribute(new Float32Array(r), 3)), 
+        t.setAttribute("uv", new Float32BufferAttribute(new Float32Array(s), 2)), 
+        t.setAttribute("color", new Float32BufferAttribute(new Float32Array(o), 3)), 
+        t;
+    }
+}
+
+function bilinearInterpolation(e) {
+    var {
+        p1: e,
+        p2: t,
+        p3: r,
+        p4: s,
+        isCentroid: o = !0
+    } = e;
+    if (o) return {
+        x: (e.pos[0] + t.pos[0] + r.pos[0] + s.pos[0]) / 4,
+        y: (e.pos[1] + t.pos[1] + r.pos[1] + s.pos[1]) / 4,
+        z: (e.pos[2] + t.pos[2] + r.pos[2] + s.pos[2]) / 4
+    };
+    throw new Error("Oops, someone had better implement non-centroid bilinear interpolation");
+}
+
+const vertexGenerator = function*(e) {
+    for (;;) yield {
+        norm: [ 0, 1, 0 ],
+        uv: [ 0, 1 ],
+        recursions: e
+    }, yield {
+        norm: [ 0, 1, 0 ],
+        uv: [ 1, 1 ],
+        recursions: e
+    }, yield {
+        norm: [ 0, 1, 0 ],
+        uv: [ 0, 0 ],
+        recursions: e
+    }, yield {
+        norm: [ 0, 1, 0 ],
+        uv: [ 0, 0 ],
+        recursions: e
+    }, yield {
+        norm: [ 0, 1, 0 ],
+        uv: [ 1, 1 ],
+        recursions: e
+    }, yield {
+        norm: [ 0, 1, 0 ],
+        uv: [ 1, 0 ],
+        recursions: e
+    };
+};
+
+export {
+    Terrainosaurus
+};
--- dist/vertex-worker.js
+++ dist/vertex-worker.js
@@ -1,1 +1,186 @@
-import"https://unpkg.com/three@0.144.0/build/three.min.js";import{SimplexNoise}from"https://unpkg.com/simplex-noise-esm@2.5.0-esm.0/dist-esm/simplex-noise.js";let simplex=null;const VERTICES_PER_SQUARE=6;function reconstructFunction(e){let s=(e=e.replace(/\S*__WEBPACK_IMPORTED_MODULE_\d+__./g,"")).split("\n");s.pop();e=s.shift();s=s.join("\n");let o=e.match(/\([^)]*\)/)[0];return o=o.replace("(","").replace(")","").split(","),Function(...o,s)}function recurseSection({vertices:s,absoluteIndex:o},t=1){for(let e=0;e<t;e++)for(let e=s.length-6;-1<e;e-=6)recursivelyGenerate.call(this,e+o)}function recursivelyGenerate(e){if(e%VERTICES_PER_SQUARE)throw new Error("The given vertex does not represent the start of a cell");var s,o;this.vertices[e]?(o=this.vertices.slice(e,e+VERTICES_PER_SQUARE),s=this.vertices[e].recursions+1,o=getSubSquares.call(this,{vertices:o,recursions:s,vertexIndex:e}),this.vertices.splice(e,VERTICES_PER_SQUARE,...o)):console.warn(`Skipping recursion: Index ${e} exceeds bounds (${this.vertices.length})`)}function getSubSquares(e){var s=e["recursions"],[o,t,r,,,n]=e.vertices,p=bilinearInterpolation({p1:o,p2:t,p3:r,p4:n}),s=vertexGenerator(s),p=[{pos:[p.x,p.y,p.z],...s.next().value},{pos:[n.pos[0],(n.pos[1]+t.pos[1])/2,p.z],...s.next().value},{pos:[p.x,(n.pos[1]+r.pos[1])/2,n.pos[2]],...s.next().value},{pos:[p.x,(n.pos[1]+r.pos[1])/2,n.pos[2]],...s.next().value},{pos:[n.pos[0],(n.pos[1]+t.pos[1])/2,p.z],...s.next().value},{...n,...s.next().value},{pos:[r.pos[0],(r.pos[1]+o.pos[1])/2,p.z],...s.next().value},{pos:[p.x,p.y,p.z],...s.next().value},{...r,...s.next().value},{...r,...s.next().value},{pos:[p.x,p.y,p.z],...s.next().value},{pos:[p.x,(n.pos[1]+r.pos[1])/2,r.pos[2]],...s.next().value},{pos:[p.x,(t.pos[1]+o.pos[1])/2,t.pos[2]],...s.next().value},{...t,...s.next().value},{pos:[p.x,p.y,p.z],...s.next().value},{pos:[p.x,p.y,p.z],...s.next().value},{...t,...s.next().value},{pos:[n.pos[0],(n.pos[1]+t.pos[1])/2,p.z],...s.next().value},{...o,...s.next().value},{pos:[p.x,(t.pos[1]+o.pos[1])/2,t.pos[2]],...s.next().value},{pos:[r.pos[0],(r.pos[1]+o.pos[1])/2,p.z],...s.next().value},{pos:[r.pos[0],(r.pos[1]+o.pos[1])/2,p.z],...s.next().value},{pos:[p.x,(t.pos[1]+o.pos[1])/2,t.pos[2]],...s.next().value},{pos:[p.x,p.y,p.z],...s.next().value}];this.generators[this.generatorSelector({topLeft:n,topRight:r,bottomLeft:t,bottomRight:o,vertexIndex:e.vertexIndex})].call(this,p,p.map(e=>simplex.noise2D(e.pos[0],e.pos[2])));return p}function bilinearInterpolation(e){var{p1:e,p2:s,p3:o,p4:t,isCentroid:r=!0}=e;if(r)return{x:(e.pos[0]+s.pos[0]+o.pos[0]+t.pos[0])/4,y:(e.pos[1]+s.pos[1]+o.pos[1]+t.pos[1])/4,z:(e.pos[2]+s.pos[2]+o.pos[2]+t.pos[2])/4};throw new Error("Oops, someone had better implement non-centroid bilinear interpolation")}self.addEventListener("message",({data:e})=>{var s,o;"recurseSection"===e.action&&(s={colors:e.colors,waterLevel:e.waterLevel,vertices:e.section.vertices,generators:e.generators.map(e=>reconstructFunction(e)),generatorSelector:reconstructFunction(e.generatorSelector),THREE:THREE},simplex=simplex||new SimplexNoise(e.seed||Math.random().toString()),o=e.levels||1,recurseSection.call(s,e.section,o),postMessage({vertices:s.vertices}))});const vertexGenerator=function*(e){for(;;)yield{norm:[0,1,0],uv:[0,1],recursions:e},yield{norm:[0,1,0],uv:[1,1],recursions:e},yield{norm:[0,1,0],uv:[0,0],recursions:e},yield{norm:[0,1,0],uv:[0,0],recursions:e},yield{norm:[0,1,0],uv:[1,1],recursions:e},yield{norm:[0,1,0],uv:[1,0],recursions:e}};
+import "https://unpkg.com/three@0.144.0/build/three.min.js";
+
+import {
+    SimplexNoise
+} from "https://unpkg.com/simplex-noise-esm@2.5.0-esm.0/dist-esm/simplex-noise.js";
+
+let simplex = null;
+
+const VERTICES_PER_SQUARE = 6;
+
+function reconstructFunction(e) {
+    let s = (e = e.replace(/\S*__WEBPACK_IMPORTED_MODULE_\d+__./g, "")).split("\n");
+    s.pop();
+    e = s.shift();
+    s = s.join("\n");
+    let o = e.match(/\([^)]*\)/)[0];
+    return o = o.replace("(", "").replace(")", "").split(","), Function(...o, s);
+}
+
+function recurseSection({
+    vertices: s,
+    absoluteIndex: o
+}, t = 1) {
+    for (let e = 0; e < t; e++) for (let e = s.length - 6; -1 < e; e -= 6) recursivelyGenerate.call(this, e + o);
+}
+
+function recursivelyGenerate(e) {
+    if (e % VERTICES_PER_SQUARE) throw new Error("The given vertex does not represent the start of a cell");
+    var s, o;
+    this.vertices[e] ? (o = this.vertices.slice(e, e + VERTICES_PER_SQUARE), s = this.vertices[e].recursions + 1, 
+    o = getSubSquares.call(this, {
+        vertices: o,
+        recursions: s,
+        vertexIndex: e
+    }), this.vertices.splice(e, VERTICES_PER_SQUARE, ...o)) : console.warn(`Skipping recursion: Index ${e} exceeds bounds (${this.vertices.length})`);
+}
+
+function getSubSquares(e) {
+    var s = e["recursions"], [ o, t, r, , , n ] = e.vertices, p = bilinearInterpolation({
+        p1: o,
+        p2: t,
+        p3: r,
+        p4: n
+    }), s = vertexGenerator(s), p = [ {
+        pos: [ p.x, p.y, p.z ],
+        ...s.next().value
+    }, {
+        pos: [ n.pos[0], (n.pos[1] + t.pos[1]) / 2, p.z ],
+        ...s.next().value
+    }, {
+        pos: [ p.x, (n.pos[1] + r.pos[1]) / 2, n.pos[2] ],
+        ...s.next().value
+    }, {
+        pos: [ p.x, (n.pos[1] + r.pos[1]) / 2, n.pos[2] ],
+        ...s.next().value
+    }, {
+        pos: [ n.pos[0], (n.pos[1] + t.pos[1]) / 2, p.z ],
+        ...s.next().value
+    }, {
+        ...n,
+        ...s.next().value
+    }, {
+        pos: [ r.pos[0], (r.pos[1] + o.pos[1]) / 2, p.z ],
+        ...s.next().value
+    }, {
+        pos: [ p.x, p.y, p.z ],
+        ...s.next().value
+    }, {
+        ...r,
+        ...s.next().value
+    }, {
+        ...r,
+        ...s.next().value
+    }, {
+        pos: [ p.x, p.y, p.z ],
+        ...s.next().value
+    }, {
+        pos: [ p.x, (n.pos[1] + r.pos[1]) / 2, r.pos[2] ],
+        ...s.next().value
+    }, {
+        pos: [ p.x, (t.pos[1] + o.pos[1]) / 2, t.pos[2] ],
+        ...s.next().value
+    }, {
+        ...t,
+        ...s.next().value
+    }, {
+        pos: [ p.x, p.y, p.z ],
+        ...s.next().value
+    }, {
+        pos: [ p.x, p.y, p.z ],
+        ...s.next().value
+    }, {
+        ...t,
+        ...s.next().value
+    }, {
+        pos: [ n.pos[0], (n.pos[1] + t.pos[1]) / 2, p.z ],
+        ...s.next().value
+    }, {
+        ...o,
+        ...s.next().value
+    }, {
+        pos: [ p.x, (t.pos[1] + o.pos[1]) / 2, t.pos[2] ],
+        ...s.next().value
+    }, {
+        pos: [ r.pos[0], (r.pos[1] + o.pos[1]) / 2, p.z ],
+        ...s.next().value
+    }, {
+        pos: [ r.pos[0], (r.pos[1] + o.pos[1]) / 2, p.z ],
+        ...s.next().value
+    }, {
+        pos: [ p.x, (t.pos[1] + o.pos[1]) / 2, t.pos[2] ],
+        ...s.next().value
+    }, {
+        pos: [ p.x, p.y, p.z ],
+        ...s.next().value
+    } ];
+    this.generators[this.generatorSelector({
+        topLeft: n,
+        topRight: r,
+        bottomLeft: t,
+        bottomRight: o,
+        vertexIndex: e.vertexIndex
+    })].call(this, p, p.map(e => simplex.noise2D(e.pos[0], e.pos[2])));
+    return p;
+}
+
+function bilinearInterpolation(e) {
+    var {
+        p1: e,
+        p2: s,
+        p3: o,
+        p4: t,
+        isCentroid: r = !0
+    } = e;
+    if (r) return {
+        x: (e.pos[0] + s.pos[0] + o.pos[0] + t.pos[0]) / 4,
+        y: (e.pos[1] + s.pos[1] + o.pos[1] + t.pos[1]) / 4,
+        z: (e.pos[2] + s.pos[2] + o.pos[2] + t.pos[2]) / 4
+    };
+    throw new Error("Oops, someone had better implement non-centroid bilinear interpolation");
+}
+
+self.addEventListener("message", ({
+    data: e
+}) => {
+    var s, o;
+    "recurseSection" === e.action && (s = {
+        colors: e.colors,
+        waterLevel: e.waterLevel,
+        vertices: e.section.vertices,
+        generators: e.generators.map(e => reconstructFunction(e)),
+        generatorSelector: reconstructFunction(e.generatorSelector),
+        THREE: THREE
+    }, simplex = simplex || new SimplexNoise(e.seed || Math.random().toString()), 
+    o = e.levels || 1, recurseSection.call(s, e.section, o), postMessage({
+        vertices: s.vertices
+    }));
+});
+
+const vertexGenerator = function*(e) {
+    for (;;) yield {
+        norm: [ 0, 1, 0 ],
+        uv: [ 0, 1 ],
+        recursions: e
+    }, yield {
+        norm: [ 0, 1, 0 ],
+        uv: [ 1, 1 ],
+        recursions: e
+    }, yield {
+        norm: [ 0, 1, 0 ],
+        uv: [ 0, 0 ],
+        recursions: e
+    }, yield {
+        norm: [ 0, 1, 0 ],
+        uv: [ 0, 0 ],
+        recursions: e
+    }, yield {
+        norm: [ 0, 1, 0 ],
+        uv: [ 1, 1 ],
+        recursions: e
+    }, yield {
+        norm: [ 0, 1, 0 ],
+        uv: [ 1, 0 ],
+        recursions: e
+    };
+};
