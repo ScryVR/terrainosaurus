e9bf8d41db890977346377e03bec7fb838d015ae
Matthew Bernardo <matthew@e-bot7.com>
2023-07-12 21:42:30 +0200

    Simplify folder structure

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,7 +1,7 @@
 import * as AFRAME from 'aframe'
-import { registerTerrainosaurusComponent } from "./classes/terrainosaurus-terrain";
+import { registerTerrainosaurusComponent } from "./terrainosaurus-terrain";
 
-const workerUrl = new URL("./classes/vertex-worker", import.meta.url)
+const workerUrl = new URL("./vertex-worker", import.meta.url)
 workerUrl.searchParams.append("isFile", "true")
 registerTerrainosaurusComponent({
   vertexWorkerUrl: workerUrl,
